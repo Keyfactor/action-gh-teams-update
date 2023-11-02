1104be99f4e403a8c15a4ef1692124c0d50c679f
Michael Henderson <mhenderson@keyfactor.com>
2023-11-02 12:03:12 -0700

    fix calling structure

--- main.js
+++ main.js
@@ -14,18 +14,17 @@ const teams = [ // Using array of arrays because team names contain hyphen chara
   ['release_builders', 'admin']
 ];
 
-async function updateTeamPermissions(owner, repo) {
+async function updateTeamPermissions(org, repoName) {
   try {
     teams.forEach(element => {
       console.log(element[0] + ' : ' + element[1])
       const team_slug = element[0];
       const permission = element[1];
-      const org = owner;
       const response = github.request("PUT /orgs/{org}/teams/{team_slug}/repos/{owner}/{repo}", {
         org,
         team_slug,
         owner,
-        repo,
+        repo: repoName,
         permission
       });
     });
